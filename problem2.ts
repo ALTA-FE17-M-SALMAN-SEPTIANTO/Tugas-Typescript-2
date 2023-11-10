1db0c34688e864ad7674bbbaceaf549807c682ec
salmanseptianto <salmanseptianto27@gmail.com>
2023-11-10 22:39:23 +0700

    add problem2

---
+++ problem2.ts
@@ -0,0 +1,24 @@
+/** @format */
+
+function exponentiation(x: number, n: number): number | string {
+  if (n < 0) {
+    return "wrong input";
+  } else if (n === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= Math.abs(n); i++) {
+      result *= x;
+    }
+    return n > 0 ? result : 1 / result;
+  }
+}
+
+// Test Cases
+console.log(exponentiation(2, 3)); // 8
+console.log(exponentiation(2, 12)); // 4096
+console.log(exponentiation(7, 2)); // 49
+console.log(exponentiation(9, 3)); // 729
+console.log(exponentiation(22, 5)); // 5153632
+console.log(exponentiation(1996, 0)); // 1
+console.log(exponentiation(4213, -3)); // "wrong input"
