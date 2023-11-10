7571e9623d4c9ec6901f508f6d7460f29186256b
salmanseptianto <salmanseptianto27@gmail.com>
2023-11-10 22:20:40 +0700

    correct problem1

--- problem1.ts
+++ problem1.ts
@@ -1,5 +1,20 @@
 /** @format */
 
-const number: "80";
-const number: "65";
+function deskripsiNilai(nilai: number): string {
+  if (nilai < 0 || nilai > 100) {
+    return "Invalid";
+  } else if (nilai >= 80) {
+    return `mendapat nilai A`;
+  } else if (nilai >= 65) {
+    return `mendapat nilai B+`;
+  } else if (nilai >= 50) {
+    return ` mendapat nilai B`;
+  } else if (nilai >= 35) {
+    return `mendapat nilai C`;
+  } else {
+    return `mendapat nilai D`;
+  }
+}
+
+console.log(deskripsiNilai(40));
 
